5abbd9a2e934cd91258a01cea4101459415b4ac1
ivanmaister7 <ivanmaister7@gmail.com>
2021-04-30 17:45:54 +0300

    Add result menu and bags fixes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,7 +7,10 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.paint.Color;
 
 public class Controller {
-    String answer;
+    private String answer;
+    private static final int NUMBER_OF_QUESTIONS = 2;
+    private double tests = 0.001;
+    private boolean first = true;
 
     @FXML
     private MenuBar menu;
@@ -17,6 +20,12 @@ public class Controller {
 
     @FXML
     private Label labelAdd;
+
+    @FXML
+    private Label labelResultText;
+
+    @FXML
+    private Label labelResult;
 
     @FXML
     private TextField fieldQuest;
@@ -55,36 +64,52 @@ public class Controller {
     private Button btnSave;
 
     @FXML
+    private Button btnRestart;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
     void initialize(){
         nextLevel();
         btnOk.setOnAction(actionEvent -> {
             RadioButton selected = (RadioButton) ans.getSelectedToggle();
             if(selected != null){
                 if(selected.getText()==answer){
-                    nextLevel();
-                    radio1.setBackground(null);
-                    radio2.setBackground(null);
-                    radio3.setBackground(null);
-                    radio4.setBackground(null);
-                    selected.setSelected(false);
+                    tests++;
+                    if(first){
+                        progressBar.setProgress(progressBar.getProgress() + 1.0/NUMBER_OF_QUESTIONS);
+                    }
+                    if(progressBar.getProgress()<1.0){
+                        nextLevel();
+                    } else {
+                        setLevelView(false);
+                        setResultView(true);
+                    }
+                    clear(selected);
                 } else {
+                    first = false;
                     selected.setBackground(new Background(new BackgroundFill(Color.RED, null, null)));
                 }
             }
         });
         btnSkip.setOnAction(actionEvent -> {
-            //load new level + skip
+            nextLevel();
+            tests++;
+            clear((RadioButton) ans.getSelectedToggle());
         });
         menu.getMenus().get(0).getItems().get(0).setOnAction(actionEvent -> {
             //go to menu
-            setLevelView(false);
             setMenuView(true);
+            setLevelView(false);
+            setResultView(false);
             setAddView(false);
         });
         menu.getMenus().get(0).getItems().get(1).setOnAction(actionEvent -> {
-            //restart level
+           restartTest();
         });
         btnStart.setOnAction(actionEvent -> {
+            restartTest();
             setLevelView(true);
             setMenuView(false);
         });
@@ -92,9 +117,37 @@ public class Controller {
             setMenuView(false);
             setAddView(true);
         });
+        btnRestart.setOnAction(actionEvent -> {
+            setLevelView(true);
+            setResultView(false);
+            restartTest();
+        });
         btnSave.setOnAction(actionEvent -> {
            // add to database
         });
+    }
+
+    private void restartTest() {
+        nextLevel();
+        tests = 0;
+        progressBar.setProgress(0);
+    }
+
+    private void clear(RadioButton selected) {
+        radio1.setBackground(null);
+        radio2.setBackground(null);
+        radio3.setBackground(null);
+        radio4.setBackground(null);
+        if(selected != null){
+            selected.setSelected(false);
+        }
+    }
+
+    private void setResultView(boolean b) {
+        labelResultText.setVisible(b);
+        labelResult.setVisible(b);
+        labelResult.setText("" + (int)(NUMBER_OF_QUESTIONS * 100/tests) + '%');
+        btnRestart.setVisible(b);
     }
 
     private void setAddView(boolean b) {
@@ -111,6 +164,7 @@ public class Controller {
 
     private void setLevelView(boolean b) {
         menu.getMenus().get(0).getItems().get(1).setVisible(b);
+        progressBar.setVisible(b);
         labelQuestion.setVisible(b);
         radio1.setVisible(b);
         radio2.setVisible(b);
@@ -128,6 +182,7 @@ public class Controller {
         radio4.setText(level.getVariants()[3]);
         labelQuestion.setText(level.getQuestion());
         answer = level.getAnswer();
+        first = true;
     }
 
 }
